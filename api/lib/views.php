5bfb197d7e2f039d06d237e9bc0facffb1c01d3a
József Sallai <jozsef@sallai.me>
2018-08-16 00:14:04 +0300

    simplify view class

--- api/lib/views.php
+++ api/lib/views.php
@@ -1,17 +1,6 @@
 <?php
 
 class View {
-  public static $viewsPath = '../api/views/';
-
-  /**
-   * getViewFile()
-   * Returns the path to the specified view
-   * @param $name
-   */
-  public static function getViewFile($name) {
-    return self::$viewsPath . $name . '.php';
-  }
-
   /**
    * render()
    * Render a view with the specified data
@@ -19,7 +8,7 @@ class View {
    * @param data
    */
   public static function render($view, $data = array()) {
-    $filename = self::getViewFile($view);
+    $filename = '../api/views/' . $view . '.php';
 
     if (!file_exists($filename)) {
       die('Unknown view! ' . $filename);
